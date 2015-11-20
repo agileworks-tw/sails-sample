b62ca14c385cee1c0b4167ed3165df344189c18e
smlsunxie <smlsun@gmail.com>
2015-11-21 00:06:27 +0800

    update

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,7 +2,6 @@ module.exports = {
   index: async (req, res) => {
     try {
       let users = await UserService.findAll();
-      sails.log.info('=== users ===', users);
       res.ok({users});
     } catch (e) {
       res.serverError(e);
