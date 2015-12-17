cedde60eace8c0ffa523cc0d13ef2a3228b87053
dmoon <dmoon.t@gmail.com>
2015-12-17 18:12:41 +0800

    search flickr image by tag is ok

--- api/services/FlickrService.js
+++ api/services/FlickrService.js
@@ -3,26 +3,20 @@ var keys = {"api_key": "c82bbfe5b720373012b6bcbae70fe0c6"}
 
 module.exports = {
 
-  search: async () => {
+  search: async (tags) => {
     try {
       console.log('test');
       let flickr = new Flickr(keys);
 
       let result = await new Promise((done) => {
-        flickr.get("photos.search", {"tags":"狗"}, (err, result) => {
-            // if (err) return console.error(err);
-            // console.log(result.photos);
-            // console.log();
-            let img = result.photos[0];
-            let url = `https://farm${img.farm}staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`;
-            console.log('url', url);
+        flickr.get("photos.search", {"tags": tags}, (err, result) => {
+            let img = result.photos.photo[0];
+            let url = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`;
+            console.log(url);
             done(url);
         });
-
       });
-      console.log(result);
-      // return result;
-      console.log('result',result);
+      return result;
 
     } catch (e) {
       throw e;
--- test/unit/services/FlickrService.spec.js
+++ test/unit/services/FlickrService.spec.js
@@ -2,24 +2,11 @@ describe('about Flickr Service operation.', function() {
 
   describe('update lile item', () => {
 
-    let like3c;
-    before(async (done) => {
-
-      like3c = await Like.create({
-        title: '測試用生活3C'
-      });
-      await Item.create({
-        itemname: "Server",
-        LikeId: like3c.id
-      })
-      done();
-    });
-
-    it.only('flickr search', async (done) => {
+    it('flickr search', async (done) => {
       try {
-
-        let url = await FlickrService.search();
-
+        let tags = 'flower';
+        let url = await FlickrService.search(tags);
+        url.should.be.an.string;
         done();
       } catch (e) {
         sails.log.error(e);
