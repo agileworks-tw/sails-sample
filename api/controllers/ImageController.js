1b20756062be7bbec1afc7a20f9bf2c99935878f
dmoon <dmoon.t@gmail.com>
2015-12-21 14:05:59 +0800

    add upload image controller

--- api/controllers/ImageController.js
+++ api/controllers/ImageController.js
@@ -1,24 +1,42 @@
 
 module.exports = {
-  index: function (req,res) {
+  index: async (req,res) => {
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
-    '<form action="http://localhost:1337/file/upload" enctype="multipart/form-data" method="post">'+
+    '<form action="http://localhost:1337/api/uploadImage" enctype="multipart/form-data" method="post">'+
     '<input type="text" name="title"><br>'+
-    '<input type="file" name="avatar" multiple="multiple"><br>'+
+    '<input type="file" name="image" multiple="multiple"><br>'+
     '<input type="submit" value="Upload">'+
     '</form>'
     )
   },
-  upload: function  (req, res) {
-    req.file('image').upload(function (err, files) {
-      if (err)
-        return res.serverError(err);
+  upload: async  (req, res) => {
+    try {
+      let config = {
+        dirname: '../../assets/img/upload'
+      };
 
-      return res.json({
-        message: files.length + ' file(s) uploaded successfully!',
-        files: files
+      let promise = new Promise((resolve, reject) => {
+        req.file('image').upload(config, async (err, files) => {
+          resolve(files);
+        });
       });
-    });
+      
+      let files = await promise.then();
+      let uploadImages = [];
+      for (let i in files) {
+        if(files[i].type.split('/')[0] == 'image') {
+          let name = files[i].filename;
+          let path = files[i].fd.split('/assets')[1];
+          uploadImages.push({
+            name: name,
+            src: path
+          });
+        }
+      }
+      return res.json(uploadImages);
+    } catch (e) {
+      res.serverError(e);
+    }
   }
 }
