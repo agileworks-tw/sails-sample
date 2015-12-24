9996e5b4b4177bf4cd85e46fbba35f84f0746979
fuyaode <dan82625@gmail.com>
2015-12-24 22:01:46 +0800

    add appn spec

---
+++ api/services/appnService.js
@@ -0,0 +1,27 @@
+var util = require('util');
+var crypto = require('crypto');
+var _ = require('lodash');
+
+let key = 'VO0CvjP9ld2lJn0hIrHO4lmt6EgWX1KqF8sz';
+let secret = 'FkiQXEWDfLcJb55oXFI6uiwonHY1XnxaNTri';
+
+module.exports = {
+
+  genCheckMacValue: (data) => {
+
+    var keys = Object.keys(data);
+    var sortedKeys = _.sortBy(keys, function(key) {
+      return key;
+    });
+
+    var uri = _.map(sortedKeys, function(key) {
+      return key + '=' + data[key];
+    }).join('&');
+    // uri = encodeURIComponent(uri);
+    uri = util.format('API_Key=%s&%s&API_Secret=%s', key, uri, secret);
+    uri = uri.toLowerCase();
+    var checksum = crypto.createHash('md5').update(uri).digest('hex').toUpperCase();
+
+    return checksum;
+  },
+}
--- test/unit/services/FlickrService.spec.js
+++ test/unit/services/FlickrService.spec.js
@@ -33,4 +33,37 @@ describe('about Flickr Service operation.', function() {
 
   });
 
+
+  describe.only('test appn', () => {
+
+    it('appn', async (done) => {
+      try {
+        let data = {
+          api: 'meeting_create',
+          host_id: 'j0smGf5Adb7EBqy2XhVfK3PN5YD0q3',
+          topic: 'test',
+          type: '1'
+        }
+        let check = appnService.genCheckMacValue(data);
+        sails.log.info("@@@@@@@@@@@",check);
+
+        data.check_value = check;
+        // data.API_Key =  'VO0CvjP9ld2lJn0hIrHO4lmt6EgWX1KqF8sz';
+        console.log(data);
+
+        let result = await request('https://zoomnow.net')
+        .post('/API/zntw_api.php')
+        .send(data);
+
+        console.log(result.body);
+
+        done();
+      } catch (e) {
+        sails.log.error(e);
+        done(e);
+      }
+    });
+
+  });
+
 });
