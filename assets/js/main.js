abf9f8b38003bd097f4a25c22b053dafa3784f2f
Kuyen <iamcxa@gmail.com>
2015-12-29 01:24:14 +0800

    improve do search action condition on click and submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,48 +11,25 @@ $(".checkTop").click(function(e) {
 });
 
 $("#formSearch").on('submit', function(e) {
-  // validation code here
-  // if (!valid) {
-  e.preventDefault();
-  // }
   var keyword = $(".searchbar-input > input").val();
-  $.ajax({
-    url: "/search/" + keyword,
-    type: "GET",
-    success: function(result) {
-      console.log(result);
-
-      showSearchResult(result);
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      console.log("xhr.status,thrownError=>", xhr.status, thrownError);
-      if (xhr.status == 403) {
-        alert("if you like this item, login please :)");
-        window.location.assign("/auth/facebook");
-      }
-    }
-  }); // end ajax
+  if (keyword) {
+    e.preventDefault();
+    goSearch(keyword);
+  } else {
+    alert("Don't forget to type something!")
+  }
 }); // end on
 
 
 // search button
 $("#search-btn").click(function(e) {
   var keyword = $(".searchbar-input > input").val();
-  console.log("search keyword is=>", keyword);
-  $.ajax({
-    url: "/search/" + keyword,
-    type: "GET",
-    success: function(result) {
-      console.log(result);
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      console.log("xhr.status,thrownError=>", xhr.status, thrownError);
-      if (xhr.status == 403) {
-        alert("if you like this item, login please :)");
-        window.location.assign("/auth/facebook");
-      }
-    }
-  }); // end ajax
+  if (keyword) {
+    e.preventDefault();
+    goSearch(keyword);
+  } else {
+    alert("Don't forget to type something!")
+  }
 }); // end click
 
 /* ====================================================================== */
@@ -65,4 +42,22 @@ function goToByScroll(id) {
       scrollTop: $("#" + id).offset().top
     },
     'slow');
-}
+}; // end goToByScroll
+
+
+function goSearch(keyword) {
+  $.ajax({
+    url: "/search/" + keyword,
+    type: "GET",
+    success: function(result) {
+      console.log(result);
+      showSearchResult(result);
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      console.log("xhr.status,thrownError=>", xhr.status, thrownError);
+      if (xhr.status == 403) {
+        // to-do print it out
+      }
+    }
+  }); // end ajax
+}; // end goSearch
