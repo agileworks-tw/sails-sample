f4ec4301ab6dfd8d2c48406dafb3b78ee5fff719
fuyaode <dan82625@gmail.com>
2016-01-11 18:49:40 +0800

    fix prod mode

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,7 +29,7 @@ var jsFilesToInject = [
 
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
-
+  '/bower/jquery/jquery.js',
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
   'bower/**/*.js',
