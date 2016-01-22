0b360074a5d7c0eed0dd767dbc097681ddb96aa8
fuyaode <dan82625@gmail.com>
2016-01-22 13:54:57 +0800

    add img resize

--- assets/js/storyView.js
+++ assets/js/storyView.js
@@ -301,13 +301,40 @@ $(function() {
 
     // shows count
     $("div.fileUpload-btn > span").text("Upload photos(" + input.get(0).files.length + ")");
-
+    var img = document.createElement("img");
     // preview selected pic.
     var reader = new FileReader();
     reader.onload = function(e) {
       // console.log("e.target.result=>", e.target.result);
       // $('img.preview').show('fast', function() {
-      $('img.preview').attr('src', e.target.result);
+      img.src = e.target.result;
+      var canvas = document.createElement("canvas");
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+
+      var MAX_WIDTH = 400;
+      var MAX_HEIGHT = 300;
+      var width = img.width;
+      var height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, width, height);
+
+      var dataurl = canvas.toDataURL("image/png");
+      $('img.preview').attr('src', dataurl);
       // });
     }
     reader.readAsDataURL(input.get(0).files[0]);
