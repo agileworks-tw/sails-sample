e67754a435962b9decb9bb37c57fac1ec60c8d21
KenYA <tikka0910@gmail.com>
2016-08-01 01:01:06 +0800

    add route of Friend url method and path.

--- config/routes.js
+++ config/routes.js
@@ -36,7 +36,11 @@ module.exports.routes = {
     view: 'index'
   },
   // homework friend
+  'get /friend': 'FriendController.index',
 
+  'post /friend/create': 'FriendController.create',
+  'put /friend/update': 'FriendController.update',
+  'delete /friend/delete/:id': 'FriendController.destroy',
   //
 
   'get /login': 'AuthController.login',
