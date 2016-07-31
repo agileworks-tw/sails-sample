68a1c4e2379dd7f93ae0016fe77ecf0c1d56967c
KenYA <tikka0910@gmail.com>
2016-07-31 17:27:41 +0800

    init default function, add getFrinend function.

--- api/controllers/FacebookController.js
+++ api/controllers/FacebookController.js
@@ -9,8 +9,11 @@ var FB = require('fb');
 var fb = new FB.Facebook();
 
 module.exports = {
-  getFriends: async function(token, userId){
+  getFriends: async (req ,res) => {
     try{
+      let token  = "EAACEdEose0cBAFK3ooNdcpDsSL7WqvDrTfGXgox2X15HsHfPKXbZA4MWL2xDTkFWjqJKowK0umNmxxrApjN9P6bVMkdgJOTikPK1ZBiCUWpY55tOZBtrvNqh9lV42NTH4e6aNaqQC8LzoAiHyJdHqZA74szHg2VoedB3Lqg2oAZDZD",
+          userId = "790001111011196"
+
       fb.setAccessToken(token);
       let result = await new Promise( function(resolve, reject){
         fb.api(`${userId}/friends`, function(res , err){
@@ -22,7 +25,8 @@ module.exports = {
         });
       });
 
-      return result;
+      res.ok({result});
+      res.end();
     }
     catch(e){
       throw e;
@@ -39,5 +43,25 @@ module.exports = {
     //   return res.friends.data;
     //
     // });
+  },
+
+  index: async(req , res) => {
+    res.ok('Hello index');
+    res.end();
+  },
+
+  create: async(req , res) => {
+    res.ok('Hello create');
+    res.end();
+  },
+
+  update: async(req , res) => {
+    res.ok('Hello update');
+    res.end();
+  },
+
+  destroy: async(req , res) => {
+    res.ok('Hello destroy');
+    res.end();
   }
 };
